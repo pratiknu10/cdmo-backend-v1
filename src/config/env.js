f52a7f2c0cc9eb64d964b6fd24bb9b5e74c06d47
pratiknu10 <pratikv@nu10.co>
2025-07-31 12:30:12 +0530

    env linked

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./src/.env" });
+
 export const port = process.env.PORT || 5002;
 export const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/myapp";
 export const jwtSecret = process.env.JWT_SECRET || "your_jwt-secret";
