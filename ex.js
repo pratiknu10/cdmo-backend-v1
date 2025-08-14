b101f387e25286aca9a8ec19ff2c4376731e386d
pratiknu10 <pratikv@nu10.co>
2025-08-14 17:29:01 +0530

    ASD

--- ex.js
+++ ex.js
@@ -1,11 +1,9 @@
-
-
-//  0. Schema Designing
-
-//  1. CRUD
-
-//  2. Nested Routing
-
-//  3. query params, params
-
-//  4. Authentication , Authorization
+const fs = require("____"); 
+const data = 'Hello, World!'; 
+fs.('output.txt', data, (err) => { 
+    if (err) { 
+        console.error('Error writing to file:', __); 
+        return; 
+    }
+console.log('Data written to file successfully'); 
+});
