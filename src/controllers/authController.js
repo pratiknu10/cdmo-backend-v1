749b9390dc80398dbdb9482461662a3d557669c9
pratiknu10 <pratikv@nu10.co>
2025-08-18 17:16:01 +0530

    ADS

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,7 +27,7 @@ export const userLogin = async (req, res) => {
     const token = jwt.sign(
       {
         id: user._id,
-        role: user.role.name,
+        role: user.role.name.toLowerCase(),
         projectAssignments: user.projectAssignments,
       },
       process.env.JWT_SECRET,
